832746cb60cee1788853b74f399a27bef58ce51a
Mario  Gamboa-Diaz <mariodiaz5531@gmail.com>
2018-02-27 21:38:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,3 +15,11 @@ function johnLennonFacts(facts){
   }
   return array
 }
+function iLoveTheBeatles(number){
+  var array = []
+  do {
+    array.push("I love the Beatles!")
+    number++
+  } while (number < 15)
+return array
+}

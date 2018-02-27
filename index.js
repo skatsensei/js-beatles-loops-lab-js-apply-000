90d8130de8f2e774cce8b3f836aef19322e9e056
Mario  Gamboa-Diaz <mariodiaz5531@gmail.com>
2018-02-27 21:35:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ function theBeatlesPlay(musicians,instruments){
   }
 return array
 }
+
+function johnLennonFacts(facts){
+  var i = 0
+  var array = []
+  while (i < facts.length{
+  array.push(facts[i] + "!!!")
+  i++
+  }
+  return array
+}
